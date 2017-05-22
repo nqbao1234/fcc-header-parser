b4883849f37fab35200428e05c891d1c3b3d00b9
Bao Nguyen <nqbao1234@yahoo.co.uk>
2017-05-23 07:54:24 +0930

    challenge completed

--- server.js
+++ server.js
@@ -4,13 +4,25 @@ let request = require('request');
 
 app.set('port', (process.env.PORT || 5000));
 
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
 
 app.get('/', function(req, res) {
-    console.log(req.connection.remoteAddress);
-    console.log(req.headers);
-    res.end("done");
+    res.render("index");
+});
+
+app.get('/api/whoami', function(req, res) {
+    let ip = req.headers["x-forwarded-for"];
+    let lang = req.headers["accept-language"].split(",")[0];
+    let os = req.headers["user-agent"];
+    os = os.substring(os.indexOf("(")+1, os.indexOf(")"));
+
+    let result = {ipaddress: ip,
+                  language: lang,
+                  software: os};
+
+    res.end(JSON.stringify(result));
+
 });
 
 app.listen(app.get('port'), function() {
