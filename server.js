4f94da56fdecd5939940bc8fa8ca3eb6b5fd4ab8
Bao Nguyen <nqbao1234@yahoo.co.uk>
2017-05-23 07:01:09 +0930

    got IP and req.headers

--- server.js
+++ server.js
@@ -2,20 +2,27 @@ let express = require('express');
 let app = express();
 let request = require('request');
 let util = require('util');
+let bodyParser = require('body-parser');
 
 app.set('port', (process.env.PORT || 5000));
 
 // app.set('views', __dirname + '/views');
 // app.set('view engine', 'pug');
 
+//use middleware to parse the request body
+app.use(bodyParser.json());
+
 app.get('/', function(req, res) {
     // res.render('index');
-    let req_full = util.inspect(req);
-    let req_str = JSON.stringify(req_full);
-    res.send(req_full);
-    console.log(req_full);
+    // let req_full = util.inspect(req);
+    // let req_full = req;
+    // let req_str = JSON.stringify(req_full);
+    // res.send(req_full);
+    // console.log(req_full);
+    console.log(req.connection.remoteAddress);
+    console.log(req.headers);
+    res.end("done");
 });
-
 
 
 // app.get('/:date_str', function (req, res) {
